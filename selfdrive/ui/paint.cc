5ea3a63eb6e9c09c1514dcc316f7c509f97150af
Comma Device <device@comma.ai>
2020-08-25 02:50:48 -0700

    removed left and right frames in ui

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -816,12 +816,12 @@ static void bb_ui_draw_measures_left(UIState *s, int bb_x, int bb_y, int bb_w ) 
     bb_ry = bb_y + bb_h;
   }
   //finally draw the frame
-  bb_h += 20;
-  nvgBeginPath(s->vg);
-    nvgRoundedRect(s->vg, bb_x, bb_y, bb_w, bb_h, 20);
-    nvgStrokeColor(s->vg, nvgRGBA(255,255,255,80));
-    nvgStrokeWidth(s->vg, 6);
-    nvgStroke(s->vg);
+  //bb_h += 20;
+  //nvgBeginPath(s->vg);
+  //  nvgRoundedRect(s->vg, bb_x, bb_y, bb_w, bb_h, 20);
+  //  nvgStrokeColor(s->vg, nvgRGBA(255,255,255,80));
+  //  nvgStrokeWidth(s->vg, 6);
+  //  nvgStroke(s->vg);
 }
 
 static void bb_ui_draw_measures_right(UIState *s, int bb_x, int bb_y, int bb_w ) {
@@ -966,12 +966,12 @@ static void bb_ui_draw_measures_right(UIState *s, int bb_x, int bb_y, int bb_w )
   }
 
   //finally draw the frame
-  bb_h += 20;
-  nvgBeginPath(s->vg);
-    nvgRoundedRect(s->vg, bb_x, bb_y, bb_w, bb_h, 20);
-    nvgStrokeColor(s->vg, nvgRGBA(255,255,255,80));
-    nvgStrokeWidth(s->vg, 6);
-    nvgStroke(s->vg);
+  //bb_h += 20;
+  //nvgBeginPath(s->vg);
+  //  nvgRoundedRect(s->vg, bb_x, bb_y, bb_w, bb_h, 20);
+  //  nvgStrokeColor(s->vg, nvgRGBA(255,255,255,80));
+  //  nvgStrokeWidth(s->vg, 6);
+  //  nvgStroke(s->vg);
 }
 
 static void bb_ui_draw_UI(UIState *s)
